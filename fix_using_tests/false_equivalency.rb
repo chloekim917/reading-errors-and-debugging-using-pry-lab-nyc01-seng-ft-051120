11b8aea6994c84af7836cd7ea5aa726dc4da5c55
Seoyoung Kim <mintholic1@gmail.com>
2020-04-22 17:30:00 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,11 +1,12 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
-   end
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
-     end
+ end
+ 
+ binding.pry
 
 
