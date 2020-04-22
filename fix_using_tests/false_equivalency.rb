986f9a42f9c289eae91b2eb8a37ccf1590327cbf
Seoyoung Kim <mintholic1@gmail.com>
2020-04-22 17:33:05 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -9,7 +9,7 @@ def prompt_user
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
-
+end
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
@@ -20,6 +20,4 @@ end
 def runner
   prompt_user
   selection(get_user_input)
-end
-
 end
