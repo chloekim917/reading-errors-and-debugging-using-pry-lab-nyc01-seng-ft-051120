c953c29c4170ccfc8a13ce4dc1a18d4f6569ee5c
Seoyoung Kim <mintholic1@gmail.com>
2020-04-22 16:41:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,5 +1,7 @@
 # don't forget to add: require 'pry'
 require 'pry'
+player1
+player2
 def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
  
@@ -10,4 +12,3 @@ def play_game(player2)
   "#{player1} is better than #{player2}"
 
 end
-   binding.pry
