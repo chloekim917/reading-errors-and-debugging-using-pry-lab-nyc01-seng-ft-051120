c09d62a393d1c500d05dd6da8e3ab3266cc105ba
Seoyoung Kim <mintholic1@gmail.com>
2020-04-22 17:32:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -15,10 +15,11 @@ def selection(num)
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
-  end
 end
 
 def runner
   prompt_user
   selection(get_user_input)
+end
+
 end
