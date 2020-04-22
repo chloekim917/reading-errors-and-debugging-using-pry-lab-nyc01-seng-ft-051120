a5f509fdc2ddef2626473ae7e64e92be3ee943f1
Seoyoung Kim <mintholic1@gmail.com>
2020-04-22 16:53:14 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,8 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-  s_letter = "s"
   if string[0] == "s"
-    10 * s_letter + string
+    10 * "s" + string
   else
      string
   end
